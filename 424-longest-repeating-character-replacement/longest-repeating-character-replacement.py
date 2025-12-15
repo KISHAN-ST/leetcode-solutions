0c3b9314f2742784da83ab24bd98487e90ac7c1c
Kishan S T <stkishan45@gmail.com>
2025-12-15 23:32:55 +0530

    Time: 131 ms (65.59%) | Memory: 15.5 MB (17.64%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        freq={}
+        left=0
+        max_freq=0
+        result=0
+        for right in range(len(s)):
+            freq[s[right]]=freq.get(s[right],0)+1
+            max_freq=max(max_freq,freq[s[right]])
+
+            while((right-left+1) - max_freq)>k:
+                freq[s[left]]-=1
+                left+=1
+            result=max(right-left+1,result)
+        return result
+
+        
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        
