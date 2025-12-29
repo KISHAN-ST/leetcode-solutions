d9b59eb683fd088e1de646d76d81896d845d5eb2
Kishan S T <stkishan45@gmail.com>
2025-12-29 20:45:07 +0530

    Time: 4 ms (51.04%) | Memory: 12.4 MB (89.60%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,16 +1,12 @@
 class Solution(object):
     def findTheDifference(self, s, t):
-       freq={}
+       result=0
+
        for ch in t:
-        freq[ch]=freq.get(ch,0)+1
+        result^=ord(ch)
+
        for ch in s:
-        freq[ch]-=1
-       for ch in freq:
-        if freq[ch]==1:
-            return ch
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
+        result^=ord(ch)
+       return chr(result)
+
         
